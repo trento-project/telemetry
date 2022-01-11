6ea57d6418e424e7ac0d9ab22c1c36013616d1e2
arbulu89 <xarbulu@suse.com>
2022-01-11 13:55:49 +0100

    Output telemetry service url

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -2,6 +2,10 @@ output "dns_name" {
   value = module.telemetry_application.dns_name
 }
 
+output "url" {
+  value = "https://${var.dns_cname}.${var.dns_zone}"
+}
+
 output "database_address" {
   value = module.database.database_address
 }
